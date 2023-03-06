c560f41758c5db8754ea86a9063b3b2400c2c87c
Dmitrii <dvvorotnikov@gmail.ru>
2023-03-06 20:46:39 +0300

    query processor adjustment for "simple classification" usage

--- processor/src/main/java/com/dmasso/multidwh/processing/CypherQueryProcessor.java
+++ processor/src/main/java/com/dmasso/multidwh/processing/CypherQueryProcessor.java
@@ -1,9 +1,9 @@
 package com.dmasso.multidwh.processing;
 
+import com.dmasso.multidwh.classification.Classifier;
 import com.dmasso.multidwh.common.enums.DbType;
 import com.dmasso.multidwh.common.interfaces.DbTyped;
 import com.dmasso.multidwh.data.QueryExecutor;
-import com.dmasso.multidwh.routing.BaseRouter;
 import com.dmasso.multidwh.translation.BaseTranslator;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +19,8 @@ import static java.util.function.Function.identity;
 @Service
 @RequiredArgsConstructor
 public class CypherQueryProcessor implements QueryProcessor<String, Iterable<?>> {
-    private final BaseRouter<String> router;
+
+    private final Classifier<String, DbType> classifier;
     private final List<BaseTranslator<String, ?>> translators;
     private final List<QueryExecutor<? super Object>> executors;
     private Map<DbType, BaseTranslator<String, ?>> typeTranslatorMap;
@@ -28,7 +29,8 @@ public class CypherQueryProcessor implements QueryProcessor<String, Iterable<?>>
 
     @Override
     public Iterable<?> execute(String query) {
-        DbType dbType = router.route(query);
+        query = prepareForProcessing(query);
+        DbType dbType = classifier.classify(query);
 
         BaseTranslator<String, ?> suitableTranslator = typeTranslatorMap.get(dbType);
         QueryExecutor<Object> suitableExecutor = typeExecutorMap.get(dbType);
@@ -43,5 +45,9 @@ public class CypherQueryProcessor implements QueryProcessor<String, Iterable<?>>
                 .collect(Collectors.toUnmodifiableMap(DbTyped::getType, identity()));
         typeExecutorMap = executors.stream()
                 .collect(Collectors.toUnmodifiableMap(DbTyped::getType, Function.identity()));
+    }
+
+    private String prepareForProcessing(String query) {
+        return query.strip().replace("\n", " ").replace("  ", " ");
     }
 }
