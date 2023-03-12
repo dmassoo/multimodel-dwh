9bbd27ccddfabc0c0c02137240b212fd9c1f1903
Dmitrii <dvvorotnikov@gmail.ru>
2023-03-12 23:50:41 +0300

    new approach to query execution (POC started)

--- processor/src/main/java/com/dmasso/multidwh/execution/SqlQueryExecutor.java
+++ processor/src/main/java/com/dmasso/multidwh/execution/SqlQueryExecutor.java
@@ -2,22 +2,75 @@ package com.dmasso.multidwh.execution;
 
 
 import com.dmasso.multidwh.common.enums.DbType;
-import org.jdbi.v3.core.Jdbi;
-import org.springframework.beans.factory.annotation.Value;
+import com.dmasso.multidwh.data.OltpConnectionProperties;
+import lombok.RequiredArgsConstructor;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import static com.dmasso.multidwh.common.enums.DbType.OLAP;
 
 @Component
+@RequiredArgsConstructor
 public class SqlQueryExecutor implements QueryExecutor<String> {
-
-    @Value("${jdbc.uri}")
-    private String uri; // TODO: 22.01.2023 create configuration properties and pass config into Jdbi
-
-    private Jdbi jdbi = Jdbi.create("jdbc:hsqldb:mem:testDB", "sa", "");
+    private final OltpConnectionProperties properties;
     @Override
     public Iterable<?> execute(String query) {
-        return jdbi.withHandle(handle -> handle.select(query).mapToMap());
+        try (Connection con =
+                     DriverManager.getConnection(properties.getUrl(), properties.getUser(), properties.getPassword())) {
+            Statement statement = con.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            ResultSetMetaData rsmd = resultSet.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+
+            // The column count starts from 1
+            HashMap<String, String> colNameToType = new HashMap<>();
+            for (int i = 1; i <= columnCount; i++) {
+                String name = rsmd.getColumnLabel(i);
+                String type = rsmd.getColumnTypeName(i);
+                colNameToType.put(name, type);
+            }
+            List<JSONObject> result = new ArrayList<>();
+            while (resultSet.next()) {
+                // TODO: 11.03.2023 it is required to get column types (java compatible) in order to get resul
+                JSONObject json = new JSONObject();
+                for (String name : colNameToType.keySet()) {
+                    String type = colNameToType.get(name);
+
+                    Class<?> javaType = mapDbColTypeToJdbcType(type);
+                    //this should call related jdbc method return
+                    // result should be put in json
+                    //callJdbcMethodByType(javaType);
+                    Object colValue = null;
+                    try {
+                        json.put(name, colValue);
+                    } catch (JSONException e) {
+                        // NoOP
+                    }
+                }
+                result.add(json);
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Class<?> mapDbColTypeToJdbcType(String colType) {
+        // TODO: 12.03.2023 define mapping from posgresql types to jdbc resultset methods
+        // such as getString(colName) in case of char/varchar/text columns
+        // String, Int, Long, Float, Double BigDecimal, Boolean, Date, Timestamp
+        // result.getObject("id", java.util.UUID.class) - for uuids
+        return null;
+    }
+
+    private void callJdbcMethodByType(Class<?> javaType) {
+
     }
 
     @Override
