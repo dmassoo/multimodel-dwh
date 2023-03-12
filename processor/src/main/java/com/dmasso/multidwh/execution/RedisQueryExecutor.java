53e12affc6d3a6946f8dcbdf386ecb7f13564284
Dmitrii <dvvorotnikov@gmail.ru>
2023-03-12 23:57:54 +0300

    approximate redis query executor implementation

---
+++ processor/src/main/java/com/dmasso/multidwh/execution/RedisQueryExecutor.java
@@ -0,0 +1,32 @@
+package com.dmasso.multidwh.execution;
+
+
+import com.dmasso.multidwh.common.enums.DbType;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.ArrayList;
+
+@Component
+public class RedisQueryExecutor implements QueryExecutor<String> {
+    // TODO: 12.03.2023 move connection props to config 
+    private final JedisPool pool = new JedisPool("localhost", 6379);
+
+    @Override
+    public Iterable<?> execute(String query) {
+        String jsonValue;
+        try (Jedis jedis = pool.getResource()) {
+            jsonValue = jedis.get(query);
+        }
+        var result = new ArrayList<String>();
+        result.add(jsonValue);
+        return result;
+
+    }
+
+    @Override
+    public DbType getType() {
+        return DbType.KEY_VALUE;
+    }
+}
