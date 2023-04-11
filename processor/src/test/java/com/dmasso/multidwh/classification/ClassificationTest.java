fcac344679cd7e590ecc7303d09e77d28cb5a582
Dmitrii <dvvorotnikov@gmail.ru>
2023-04-11 23:49:44 +0300

    test cases for classification

--- processor/src/test/java/com/dmasso/multidwh/classification/ClassificationTest.java
+++ processor/src/test/java/com/dmasso/multidwh/classification/ClassificationTest.java
@@ -7,9 +7,9 @@ public class ClassificationTest {
     private static final Classifier<String, DbType> classifier = new SimpleWorkloadTypeClassifier();
 
     @Test
-    public void testRelationshipOrientedQuery() {
+    public void testRelationshipOrientedQuery_returnsGraph() {
         String query = """
-                MATCH (n:Label)-[r]->(m:Label)
+                MATCH (n:person)-[r]->(m:person)
                 WHERE r:R1|R2
                 RETURN r.name AS name
                 """;
@@ -18,19 +18,97 @@ public class ClassificationTest {
     }
 
     @Test
-    public void testSeveralRelationships() {
-        String query = "MATCH (n:Label)-[r:(R1|Rel2)&Rel3]→(m:Label) RETURN r.name AS name";
+    public void testSeveralRelationships_returnsGraph() {
+        String query = """
+                MATCH (p:person)-[r:ACTED_IN|DIRECTED|PRODUCED]-(m:movie) WHERE p.id = 55321
+                RETURN m
+                """;
         DbType result = classifier.classify(query);
         assert result == DbType.GRAPH;
     }
 
     @Test
-    public void testVariableLength() {
-        var query = "MATCH p = (a)-[:KNOWS*]->()\n" +
-                "RETURN relationships(p) AS r";
+    public void testVariableLength_returnsGraph() {
+        var query = """
+                MATCH pth = (p:person)-[*1..5]-(pe:person)
+                RETURN pth AS path
+                """;
         DbType result = classifier.classify(query);
         assert result == DbType.GRAPH;
     }
+
+    @Test
+    public void testKVPattern_returnsKV() {
+        var query = "MATCH (m:movie) WHERE m.id = 654321 RETURN m;";
+        DbType result = classifier.classify(query);
+        assert result == DbType.KEY_VALUE;
+    }
+
+    @Test
+    public void testKVPattern_noKVMetadata() {
+        var query = "MATCH (p:person) WHERE p.id = 654321 RETURN p;";
+        DbType result = classifier.classify(query);
+        assert result == DbType.OLTP;
+    }
+
+    @Test
+    public void testOLAP_Scan() {
+        var query = "MATCH (m:movie) WHERE m.title CONTAINS 'Love' RETURN m.id";
+        DbType result = classifier.classify(query);
+        assert result == DbType.OLAP;
+    }
+
+    @Test
+    public void testOLAPAgg() {
+        var query = """
+                MATCH (p:person)-[:DIRECTED]-(m:movie)
+                WHERE p.name = 'Quentin Tarantino' RETURN count(*) as cnt
+                """;
+        DbType result = classifier.classify(query);
+        assert result == DbType.OLAP;
+    }
+
+    @Test
+    public void testOLAP_LowCard() {
+        var query = """
+                MATCH (m:movie)
+                WHERE m.released = 1977 AND m.title STARTS WITH 'Love'
+                RETURN m.id, m.released, m.title, m.tagline
+                """;
+        DbType result = classifier.classify(query);
+        assert result == DbType.OLAP;
+    }
+
+    @Test
+    public void testOLAP_fewColsSelectInColOriented() {
+        var query = "MATCH (m:movie) WHERE m.released > 2000 RETURN m.title";
+        DbType result = classifier.classify(query);
+        assert result == DbType.OLTP;
+    }
+
+    @Test
+    public void testCompareIndices() {
+        var query = """
+                MATCH (m:movie)
+                WHERE m.title = 'Spider Man' and m.released = 2000
+                RETURN m.id, m.released, m.title, m.tagline
+                """;
+        DbType result = classifier.classify(query);
+        assert result == DbType.OLTP;
+    }
+
+    @Test
+    public void testOLTP() {
+        var query = """
+                MATCH (m:movie)
+                WHERE m.title STARTS WITH 'Love'
+                RETURN m.id, m.released, m.title, m.tagline
+                """;
+        DbType result = classifier.classify(query);
+        assert result == DbType.OLTP;
+    }
+
+
 
 
 }
